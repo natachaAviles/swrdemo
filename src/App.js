a42e9de91623073d74ad1f46dcd6f941d55c91b5
natacha aviles <avilesnatacha1@gmail.com>
2020-09-11 11:54:40 -0300

    feat: add base demo functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import useSWR from 'swr'
+import axios from 'axios'
+
+const fetcher = url => axios.get(url).then(res => res.data)
+
+function Information () {
+  const { data, error } = useSWR('https://api.xor.cl/ts/?paradero=pb1283', fetcher, { refreshInterval: 1000, revalidateOnFocus: true, refreshWhenOffline: true })
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  return (
+    <div className='app-container'>
+      <p>Parada: {data.descripcion}</p>
+      {data.servicios.map((servicio, index) => (
+        <div className='app__block' key={index}>
+          <p>Servicio: {servicio.servicio}</p>
+          <p>Patente: {servicio.patente}</p>
+          <p className={!servicio.descripcionError ? 'app__text--normal' : 'app__text--error' }>
+            Tiempo de espera: {!servicio.tiempo ? servicio.descripcionError : servicio.tiempo}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Information />
     </div>
   );
 }
